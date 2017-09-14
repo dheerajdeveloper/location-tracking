ebb0d52c6d90e5412153da1c7db1f6900e20cf93
Dheeraj Tyagi <dheeraj.tyagi@paytm.com>
2017-09-14 08:25:59 +0530

    location tracking android app setup

---
+++ app/src/main/java/com/example/dheeraj/location_tracking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dheeraj.location_tracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
